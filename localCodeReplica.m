80a2c467a9af336f1b4c3e8912996c4e9ed7e584
arnauochoa <arnau.ochoa@gmail.com>
2020-12-09 18:56:38 +0100

    2nd order PLL implemented

--- localCodeReplica.m
+++ localCodeReplica.m
@@ -4,8 +4,8 @@ function [cm] = localCodeReplica(prn, kDelay, tC, tVec)
 
 code        = ca_code(prn);
 % Code samples
-c          = create_code_samples(code, tVec/tC); %normalize tvec wrt chip duration
+c           = create_code_samples(code, tVec/tC); %normalize tvec wrt chip duration
 % Shift in time
-cm          = circshift(c, kDelay);%-5220 % extra -1 accounts for first sample which is code bit 1023 due to t=0
+cm          = circshift(c, kDelay); % extra -1 accounts for first sample which is code bit 1023 due to t=0
 % cm  = [cm(end-kDelay+1:end) cm(1:end-kDelay)];
 end
