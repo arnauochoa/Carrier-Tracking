7373f022287a9953d77ef9b4a59b9a15c4d713a1
arnauochoa <arnau.ochoa@gmail.com>
2020-12-24 19:42:15 +0100

    plotting histogram of Vd

--- syntheticSignal.m
+++ syntheticSignal.m
@@ -1,4 +1,4 @@
-function [signal] = syntheticSignal(prn, kDelay, A, cno, tC, fIF, phi, tVec, fDoppler)
+function [signal] = syntheticSignal(prn, kDelay, A, cno, tC, fIF, phi, tVec, fDoppler, B)
 % This function generates a synthetic signal with the specified parameters
 
 % Initializations
@@ -6,12 +6,13 @@ function [signal] = syntheticSignal(prn, kDelay, A, cno, tC, fIF, phi, tVec, fDo
 [cm] = localCodeReplica(prn, kDelay, tC, tVec);
 
 % Noise
-cno = 10^(cno/10);
-noiseSTD    = sqrt( (A^2) / (4*cno) );
-bf          = normrnd(0, noiseSTD, size(tVec));
+cno         = 10^(cno/10);
+N0          = (A^2) / (2*cno);
+N           = N0 * B;
+bf          = normrnd(0, sqrt(N), size(tVec));
 
 % Signal generation
-signal  = A * cm .* cos(2*pi*(fIF+fDoppler)*(tVec) + phi) + bf;
+signal  = A * cm .* cos(2*pi*(fIF+fDoppler)*(tVec) - phi) + bf;
 
 % Quantization (not necessary)
 % signal(signal <= -0.5) = -1;
